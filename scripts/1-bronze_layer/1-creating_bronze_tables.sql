469ad0e679d1c56cecdb5ec97ab0923f02e35112
abdelrahmanheiba48 <abdelrahmanheiba48@gmail.com>
2026-06-29 18:39:33 +0300

    Update and rename placeholder to 1-creating_bronze_tables.sql

---
+++ scripts/1-bronze_layer/1-creating_bronze_tables.sql
@@ -0,0 +1,88 @@
+-- ============================================
+-- DDL Script: Bronze Layer Tables
+-- Purpose:    Create all raw staging tables
+-- ============================================
+USE DataWarehouse
+GO
+-- ============================================
+-- Table: bronze.crm_cust_info
+-- Source: CRM System
+-- Description: Raw customer information
+-- ============================================
+PRINT '>> Creating table: bronze.crm_cust_info';
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info(
+	cst_id NVARCHAR(50),
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status CHAR(1),
+	cst_gndr CHAR(1),
+	cst_create_date DATE
+);
+PRINT '>> Done: bronze.crm_prd_info created successfully';
+GO
+
+PRINT '>> Creating table: bronze.crm_prd_info';
+
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost DECIMAL(10,2),
+	prd_line VARCHAR(5),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
+PRINT '>> Done: bronze.crm_prd_info created successfully';
+GO
+
+PRINT '>> Creating table: bronze.crm_sales_details';
+
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id NVARCHAR(50),
+	sls_order_dt INT,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales DECIMAL(10,2),
+	sls_quantity INT,
+	sls_price DECIMAL(10,2)
+);
+PRINT '>> Done: bronze.crm_sales_details created successfully';
+GO
+PRINT '>> Creating table: bronze.erp_CUST_AZ12';
+
+DROP TABLE IF EXISTS bronze.erp_CUST_AZ12;
+CREATE TABLE bronze.erp_CUST_AZ12(
+	CID NVARCHAR(50),
+	BDATE DATE,
+	GEN NVARCHAR(10)
+);
+PRINT '>> Done: bronze.erp_CUST_AZ12 created successfully';
+GO
+PRINT '>> Creating table: bronze.erp_LOC_A101';
+
+DROP TABLE IF EXISTS bronze.erp_LOC_A101;
+CREATE TABLE bronze.erp_LOC_A101(
+	CID NVARCHAR(50),
+	CNTRY NVARCHAR(50)
+);
+PRINT '>> Done: bronze.erp_LOC_A101 created successfully';
+GO
+PRINT '>> Creating table: bronze.erp_PX_CAT_G1V2';
+
+DROP TABLE IF EXISTS bronze.erp_PX_CAT_G1V2;
+CREATE TABLE bronze.erp_PX_CAT_G1V2(
+	ID NVARCHAR(50),
+	CAT NVARCHAR(50),
+	SUBCAT NVARCHAR(50),
+	MAINTENANCE NVARCHAR(50)
+);
+PRINT '>> Done: bronze.erp_PX_CAT_G1V2 created successfully';
+GO
