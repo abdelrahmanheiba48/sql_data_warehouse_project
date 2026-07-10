3eb5cac5a8f5af0daf6e4b55202d10949fa3098c
abdelrahmanheiba48 <abdelrahmanheiba48@gmail.com>
2026-07-10 18:56:37 +0300

    Update and rename 2-DDL_bronze.sql to 2-proc_load_bronze.sql

--- scripts/1-bronze_layer/2-proc_load_bronze.sql
+++ scripts/1-bronze_layer/2-proc_load_bronze.sql
@@ -1,3 +1,22 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
 CREATE OR  ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @START_TIME DATETIME , @END_TIME DATETIME , @PROC_START_TIME DATETIME , @PROC_END_TIME DATETIME 
